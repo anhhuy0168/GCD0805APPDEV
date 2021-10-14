aaeac708735749b4d0c467a65240847c518f9898
huy nguyen anh <anhhuy123zxc123@gmail.com>
2021-10-14 14:30:48 +0700

    Create Category Table

---
+++ appdev/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace appdev.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ appdev/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace appdev.Models
+{
+    public class Category
+    {
+		public int ID { get; set; }
+
+		[Required]
+		[StringLength(255)]
+		public string Name { get; set; }
+
+		[Required]
+		[StringLength(255)]
+		public string Description { get; set; }
+	}
+}
