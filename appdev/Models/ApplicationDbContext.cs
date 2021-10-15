ab2be8e91dacec4a1658ac2767ea593fe4c22c95
huy nguyen anh <anhhuy123zxc123@gmail.com>
2021-10-15 09:22:14 +0700

    Create Staff Table

--- appdev/Models/ApplicationDbContext.cs
+++ appdev/Models/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace appdev.Models
         public DbSet<Category> Category { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<TraineeUser> TraineeUsers { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
---
+++ appdev/Models/Staff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace appdev.Models
+{
+    public class Staff
+    {
+        [Key]
+        [ForeignKey("User")]
+        //khoa ngoai vs bang user
+        public string StaffId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        //for validation
+        [Required]
+        [StringLength(255)]
+
+        public string FullName { get; set; }
+        [Required]
+        [StringLength(255)]
+
+        public string Age { get; set; }
+        [Required]
+        [StringLength(255)]
+
+        public string Address { get; set; }
+    }
+}
